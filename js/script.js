056747d188ce65128ee2903f695f24b702860a29
oduvanka <qcatjul@gmail.com>
2018-12-05 02:47:22 +0300

    Создание и сортировка переписаны в виде функций с рекурсией

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var	container = document.querySelector('.container'),
-	countBubbles = 0,
+var	contentBubbles = document.querySelector('.contentBubbles'),
 	arrD = [];
 
 // Возвращает случайное целое число между min (включительно) и max (не включая max)
@@ -9,124 +8,131 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 	
-function createBubbles () {
-	var contentWeight = container.clientWidth - 50,
-		minD = 20,
+function createBubble (i = 0, sumD = 0) {
+	var contentWeight = contentBubbles.clientWidth,
+		minD = ((contentWeight > 1000) ? (Math.floor(contentWeight / 30)) : 20),
 		maxD = Math.floor(contentWeight / 10),
-		sumD = 0,
 		newD = 0,
-		i = 0;
+		spaceD = 40;
 	
-	//Создаём массив диаметров шаров
-	while (sumD < contentWeight) {
-		newD = getRandomInt(minD, maxD);
-		sumD += newD;
-		sumD += 10; //Учтём отступы
-		if (sumD < contentWeight) {
-			arrD[i] = newD;
-			i++;
-		}
+	//Записываем в массив диаметр нового шара
+	newD = getRandomInt(minD, maxD);	
+	arrD[i] = newD;
+	sumD += newD;
+	sumD += spaceD; //Учтём отступы
+	
+	//Создаём шар с соответствующим диаметром
+	var newBubble = document.createElement("div");
+	newBubble.classList.add('bubble');
+	
+	/*Цвет шара*/
+	var colorBubble = 'bubbleYellow';
+	if (i%4 === 0) {
+		colorBubble = 'bubbleRed';
 	}
-	countBubbles = i;
+	else if (i%3 === 0) {
+		colorBubble = 'bubbleBlue';
+	}
+	else if (i%2 === 0) {
+		colorBubble = 'bubbleGreen';
+	}
+	newBubble.classList.add(colorBubble);
 	
-	//Создаём шары с соответствующими диаметрами
-	for (var i = 0; i < countBubbles; i++) {
-		var newBubble = document.createElement("div");
-		newBubble.classList.add('bubble');
-		
-		/*Цвет шара*/
-		var colorBubble = 'bubbleYellow';
-		if (i%4 === 0) {
-			colorBubble = 'bubbleRed';
-		}
-		else if (i%3 === 0) {
-			colorBubble = 'bubbleBlue';
-		}
-		else if (i%2 === 0) {
-			colorBubble = 'bubbleGreen';
-		}
-		newBubble.classList.add(colorBubble);
-		
-		var bublleOptions = arrD[i] + 'px';
-		newBubble.style.width = bublleOptions;
-		newBubble.style.height = bublleOptions;
-		newBubble.style.borderRadius = bublleOptions;
-		
-		newBubble.textContent = arrD[i];	//Для отладки
-		container.appendChild(newBubble);
+	var bublleOptions = arrD[i] + 'px';
+	newBubble.style.width = bublleOptions;
+	newBubble.style.height = bublleOptions;
+	newBubble.style.borderRadius = bublleOptions;
+	
+	newBubble.textContent = arrD[i];	//Для отладки выводим число на шаре
+	contentBubbles.appendChild(newBubble);
+	
+	if ((contentWeight - sumD) > (maxD + spaceD)) {
+		//Если оставшееся свободное место больше, чем максимальный шарик с учётом отступов
+		i++;
+		createBubble(i, sumD);
 	}
 }
 
-function sortBubbles () {
-	var i = 0;
-	var sortI_id = setTimeout(function sortI (i) 
+
+/*Меняет местами 2 шара и проверяет, нужны ли ещё перестановки
+Параметры:
+k			- счётчик элементов, увеличивается на 1, обнуляется когда завершён перебор всех элементов (проход)
+flagWasSwap	- флаг, отмечает что за проход была перестановка, сбрасывается перед новым проходом
+n			- индекс последнего в проходе элемента, уменьшается на 1 когда завершён проход
+startTime		- таймаут выполнения функций, секунды, переводится в мс и увеличивается перед вызовом setTime
+*/
+function sortBubbles (k = 0, flagWasSwap = false, n = arrD.length-1, startTime = 0) {
+	var i = k;
+	if (arrD[i] > arrD[i+1]) {
+		//Меняем местами диаметры
+		var tempD = arrD[i];
+		arrD[i] = arrD[i+1];
+		arrD[i+1] = tempD;
+		
+		flagWasSwap = true;
+	}
+	
+	startTime += 1;
+	setTimeout(function() 
 	{
-		var flagWasSwap = false;
-		var j = 0;
-		var sortJ_id = setTimeout(function sortJ (j) 
+		var bubbles = document.querySelectorAll('.bubble');
+		console.log(i);
+		//Выделяем шары
+		bubbles[i].classList.add('bubbleInFocus');
+		bubbles[i+1].classList.add('bubbleInFocus');
+	}, startTime*1000);	
+		
+	if (flagWasSwap) {	
+		startTime += 1;
+		setTimeout(function() 
 		{
 			var bubbles = document.querySelectorAll('.bubble');
-			var bubbleLeft = bubbles[j];
-			var bubbleRight = bubbles[j+1];
 			
-			//Выделяем сравниваемые шары
-			bubbleLeft.classList.add('bubbleInFocus');
-			bubbleRight.classList.add('bubbleInFocus');
+			//Выделяем неправильные шары
+			bubbles[i].classList.add('bubbleFalse');
+			bubbles[i+1].classList.add('bubbleFalse');
+		}, startTime*1000, i);
+		
+		startTime += 1;
+		setTimeout(function() 
+		{
+			var bubbles = document.querySelectorAll('.bubble');
 			
-			if (arrD[j] > arrD[j+1]) {
-				//Меняем местами диаметры
-				var tempD = arrD[j];
-				arrD[j] = arrD[j+1];
-				arrD[j+1] = tempD;
-				
-				setTimeout(function() 
-				{
-					bubbleLeft.classList.add('bubbleFalse');
-					bubbleRight.classList.add('bubbleFalse');
-				}, 500);
-				
-				setTimeout(function() 
-				{
-					//Меняем местами шары
-					//bubbleRight.classList.add('moveBubbleToLeft');
-					//bubbleLeft.classList.add('moveBottomRight');
-					container.removeChild(bubbleRight);
-					container.insertBefore(bubbleRight, bubbleLeft);
-				}, 1000);
-				
-				flagWasSwap = true;
-			}
-			
-			setTimeout(function() 
-			{
-				bubbleLeft.classList.remove('bubbleInFocus');
-				bubbleLeft.classList.remove('bubbleFalse');
-				//bubbleLeft.classList.remove('moveBottomRight');
-				
-				bubbleRight.classList.remove('bubbleInFocus');
-				bubbleRight.classList.remove('bubbleFalse');
-				//bubbleRight.classList.remove('moveBubbleToLeft');
-			}, 2000);
-			
-			j++;
-			if (j === countBubbles-i) {
-				clearTimeout(sortJ_id);
-				return;
-			}
-			setTimeout(sortJ, 2000, j);
-		}, 1000, 0);
-		if (!flagWasSwap) {
-			//Не было ни одного обмена за проход
-			//Досрочно выходим из сортировки
-			clearTimeout(sortI_id);
+			//Меняем местами шары
+			contentBubbles.removeChild(bubbles[i+1]);
+			contentBubbles.insertBefore(bubbles[i+1], bubbles[i]);
+		}, startTime*1000, i);
+	}
+	
+	//Снимаем выделение с шаров
+	startTime += 1;
+	setTimeout(function() 
+	{
+		var bubbles = document.querySelectorAll('.bubble');
+		
+		bubbles[i].classList.remove('bubbleInFocus');
+		bubbles[i].classList.remove('bubbleFalse');
+		
+		bubbles[i+1].classList.remove('bubbleInFocus');
+		bubbles[i+1].classList.remove('bubbleFalse');
+	}, startTime*1000, i);
+	
+	k++;
+	
+	if ((k === n) && (!flagWasSwap)) {
+		//Дошли до последнего шара и не было перестановок
+		//Это финал, наряжаем ёлку
+	}
+	else {
+		if (k === n) {
+			//Дошли до последнего шара, перестановки были, поэтому надо перебирать заново
+			k = 0;
+			n--;
+			flagWasSwap = false;	
 		}
-		i++;
-		if (i === countBubbles) return;
-		setTimeout(sortI, 2000*countBubbles, i);
-	}, 1000, 0);
+		sortBubbles(k, flagWasSwap, n, startTime);
+	}
 }
-
-
 
 
 
